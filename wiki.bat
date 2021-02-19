9486ab05f7fd6478c5165ca85b6c4bf783ed13ff
quentin <quentin@minster.io>
2021-02-19 01:05:51 +0100

    switch to --listen, add argument for lazy loading

--- wiki.bat
+++ wiki.bat
@@ -3,4 +3,6 @@ SET Port=%1
 IF "%~1"=="" SET Port=8080
 SET IP=%2
 IF "%~2"=="" SET IP=127.0.0.1
-tiddlywiki . --server %Port% $:/core/save/all text/plain text/html "" "" %IP%
+SET Root=$:/core/save/all
+IF "%~3"=="lazy" SET Root=$:/core/save/lazy-all
+tiddlywiki . --listen host=%IP% port=%Port% root-tiddler=%Root%
--- wiki.sh
+++ wiki.sh
@@ -4,5 +4,7 @@ port="$1"
 [[ -z "$port" ]] && port=8080
 ip="$2"
 [[ -z "$ip" ]] && ip=127.0.0.1
+root='$:/core/save/all'
+[[ "$3" == lazy ]] && root='$:/core/save/lazy-all'
 
-tiddlywiki . --server "$port" \$:/core/save/all text/plain text/html "" "" "$ip"
+tiddlywiki . --listen host="$ip" port="$port" root-tiddler="$root"
